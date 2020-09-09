becf5b9012f0419a63cd049183fb417b0ff852ba
최우석 <58156742+WooSeok-03@users.noreply.github.com>
2020-09-09 13:07:31 +0900

    [Create]NFC

---
+++ Arduino/NFC/NFC.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN   9 
+#define SS_PIN    10
+
+
+MFRC522 mfrc(SS_PIN, RST_PIN);
+
+void setup(){
+  Serial.begin(9600);
+  SPI.begin();
+
+  mfrc.PCD_Init();                               
+}
+
+void loop(){
+  if ( !mfrc.PICC_IsNewCardPresent() || !mfrc.PICC_ReadCardSerial() ) {   
+    delay(500);
+    return;
+  }
+
+  Serial.print(mfrc.uid.uidByte[0]);
+}
